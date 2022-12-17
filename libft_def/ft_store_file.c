dda878d066b3ec51e7395a9bb2e553e95a77880a
Vicente Garcia Andrade <vicgarci@c3r8s5.42madrid.com>
2022-12-17 14:44:45 +0100

    leaks_free

--- libft_def/ft_store_file.c
+++ libft_def/ft_store_file.c
@@ -6,29 +6,42 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 12:26:09 by vicgarci          #+#    #+#             */
-/*   Updated: 2022/12/17 13:29:45 by vicgarci         ###   ########.fr       */
+/*   Updated: 2022/12/17 14:42:50 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static t_bool	ft_store_line(char **dst, char *s)
+{
+	char	*temp;
+
+	if (!(*dst))
+		*dst = s;
+	else
+	{
+		temp = ft_strjoin((*dst), s);
+		if (!temp)
+			return (false);
+		*dst = temp;
+		free(temp);
+	}
+	free(s);
+	return (true);
+}
+
 t_bool	ft_store_file(int fd, char **target)
 {
 	char	*s;
-	char	*temp;
+	char	*dst;
 
 	s = ft_get_next_line(fd);
 	if (!s)
 		return (false);
 	while (s)
 	{
-		if (s)
+		if (s && ft_store_line(&dst, s))
 		{
-			temp = ft_strjoin(*target, s);
-			*target = temp;
-			free(s);
-			if (!(*target))
-				return (false);
 			s = ft_get_next_line(fd);
 		}
 		else
