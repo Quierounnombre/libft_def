06fd02f1daf8294aef0de62d79afb2b41621d4ce
Vicente Garcia Andrade <vicgarci@c3r8s5.42madrid.com>
2022-12-17 19:03:23 +0100

    SEG_FAULT working on it

--- libft_def/ft_store_file.c
+++ libft_def/ft_store_file.c
@@ -6,50 +6,114 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 12:26:09 by vicgarci          #+#    #+#             */
-/*   Updated: 2022/12/17 14:42:50 by vicgarci         ###   ########.fr       */
+/*   Updated: 2022/12/17 19:02:10 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_bool	ft_store_line(char **dst, char *s)
+static char	**load(char **s, char *s2)
 {
-	char	*temp;
+	int		i;
+	char	**new;
 
-	if (!(*dst))
-		*dst = s;
-	else
+	i = 0;
+	while ((s[i]) != NULL)
+		i++;
+	new = (char **)malloc(sizeof(char *) * (i + 1));
+	if (new)
 	{
-		temp = ft_strjoin((*dst), s);
-		if (!temp)
-			return (false);
-		*dst = temp;
-		free(temp);
+		i = 0;
+		while ((s[i]) != NULL)
+		{
+			new[i] = s[i];
+			i++;
+		}
+		new[i] = s2;
 	}
 	free(s);
-	return (true);
+	return (new);
+}
+
+static t_bool	free_all(char **s)
+{
+	int	i;
+
+	i = 0;
+	while ((s[i]) != NULL)
+	{
+		free(s[i]);
+		i++;
+	}
+	free(s);
+	return (false);
+}
+
+static void	store(char **dst, char **s)
+{
+	int		i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i][j])
+		{
+			(*dst)[j] = s[i][j];
+			j++;
+		}
+		free(s[i]);
+	}
+	i++;
+	(*dst)[i] = '\0';
+	free(s);
+}
+
+static char	*convert(char **s)
+{
+	int		i;
+	size_t	len;
+	char	*dst;
+
+	i = 0;
+	len = 0;
+	while ((s[i]) != NULL)
+	{
+		len += strlen(s[i]);
+		i++;
+	}
+	dst = (char *)malloc(sizeof(char) * (len + 1));
+	if (dst)
+		store(&dst, s);
+	return (dst);
 }
 
 t_bool	ft_store_file(int fd, char **target)
 {
 	char	*s;
-	char	*dst;
+	char	**store;
 
 	s = ft_get_next_line(fd);
-	if (!s)
-		return (false);
 	while (s)
 	{
-		if (s && ft_store_line(&dst, s))
-		{
-			s = ft_get_next_line(fd);
-		}
-		else
-		{
-			free(*target);
-			return (false);
-		}
+		store = load(store, s);
+		if (!store)
+			return (free_all(store));
+		s = ft_get_next_line(fd);
 	}
-	close(fd);
+	(*target) = convert(store);
 	return (true);
+}
+
+int main(void)
+{
+	char	*s;
+	int		fd = open("./text", O_RDONLY);
+
+	if (ft_store_file(fd, &s))
+	{
+		system("Leaks PRUEBAS");
+		printf("%s\n", s);
+	}
 }
